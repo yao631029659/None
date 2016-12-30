32a625c407f5036cea74aaa18e61f23f9274a9eb
yoyo3 <yao631029659@126.com>
2016-12-30 22:34:19 +0800

    用自己的代码替换了 flask-table

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #coding:utf-8
 from flask import Flask
-# 下面两个是给表格用的
-from flask_table import Table, Col
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
@@ -17,20 +15,15 @@ class FamousProduct(db.Model):
     def __init__(self,name,description):
         self.name=name
         self.description=description
-
-    name=db.Column(db.String(255),primary_key=True)
+    id = db.Column(db.Integer(),primary_key=True)
+    name = db.Column(db.String(10))
     description=db.Column(db.String(255))
-
+    web=db.Column(db.String(255))
+    band=db.Column(db.Boolean)
+    add_time=db.Column(db.DateTime)
+    add_person=db.Column(db.String(10))
     def __repr__(self):
         return '<FamousProduct %s %s>'%(self.name,self.description)
-
-# html表格
-class ShowTable(Table):
-    # 这个是保留字 会给table增加class属性
-    classes = ['table','table-border','table-sriped','table-hover','table-condensed']
-    # 这个表两个列
-    name = Col('Name')
-    description = Col('Description')
 
 
 # wtf 验证类
@@ -40,33 +33,52 @@ class CheckForm(Form):
         validators=[DataRequired()]
     )
 
-
-
 @app.route('/',methods=('get','post'))
 def postdata():
-
     # 在那里调用就在哪里生成  要不然会报错哦 wit实例
     form = CheckForm()
     # 这句话的意思是 通过验证才能执行啊
-    if form.validate_on_submit():
-        print('if 被执行')
-        search='%'+form.name.data+'%'
-        items=FamousProduct.query.filter(FamousProduct.name.like(search)).all()
+    # if form.validate_on_submit():
+    #     print('if 被执行')
+    #     search='%'+form.name.data+'%'
+    #     items=FamousProduct.query.filter(FamousProduct.name.like(search)).all()
+    #
+    # else:
+    #     print('else 被执行')
+    #     items=FamousProduct.query.all()
+    famousproduct=FamousProduct.query.all()
+    endpoint='postdata'
 
-    else:
-        print('else 被执行')
-        items=FamousProduct.query.limit(10).all()
+    print(type(id))
     return render_template(
         'home.html',
         # 把table传输给view了
-        table=ShowTable(items),
+        endpoint=endpoint,
+        data=famousproduct,
         form=form
+
     )
-    # print(table.__html__())
 
 @app.route('/addband',methods=('post','get'))
 def addband():
     pass
+
+@app.route('/edit/<int:id>',methods=('post','get'))
+def edit(id):
+    id=id
+    return render_template(
+        'edit.html',
+        id=id
+    )
+
+
+@app.route('/delete/<int:id>',methods=('post','get'))
+def delete():
+    print('删除成功')
+
+@app.route('/detail/<int:id>',methods=('post','get'))
+def detail():
+    print('明细')
 
 if __name__ == '__main__':
     app.run()
