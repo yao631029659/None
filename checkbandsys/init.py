003a1afcae1c9f7f0dec989d15922b33b363313b
yoyo3 <yao631029659@126.com>
2017-01-05 09:32:59 +0800

    新增工厂模式

--- checkbandsys/init.py
+++ checkbandsys/init.py
@@ -4,14 +4,17 @@ from checkbandsys.controllers.checkband import checkband_blueprint
 from checkbandsys.models import db
 from checkbandsys.config import DevConfig
 from flask_uploads import configure_uploads,UploadSet,IMAGES
-app = Flask(__name__)
-app.config.from_object(DevConfig)
-db.init_app(app)
-@app.route('/')
-def index():
-    return redirect(url_for('checkband.postdata'))
 
-app.register_blueprint(checkband_blueprint)
+def create_app(obj_name):
+    app = Flask(__name__)
+    app.config.from_object(obj_name)
+    db.init_app(app)
+    @app.route('/')
+    def index():
+        return redirect(url_for('checkband.postdata'))
+    app.register_blueprint(checkband_blueprint)
+    return app
 if __name__=="__main__":
+    app=create_app('checkbandsys.config.DevConfig')
     app.run()
 
